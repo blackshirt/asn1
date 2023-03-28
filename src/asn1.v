eabf67cfcb50553837bb41da6d2e7f14d0efad20
blackshirt muslim <blackshirtmuslim@yahoo.com>
2023-03-28 06:02:37 +0000

    add parser o core asn1 file"

--- src/asn1.v
+++ src/asn1.v
@@ -48,11 +48,130 @@ pub fn (enc Encoder) contents() ![]u8 {
 	return out
 }
 
+// der_decode is main routine to do parsing of DER encoded data.
+// Its accepts bytes arrays encoded in DER in `src` params and returns `Encoder` interfaces object,
+// so, you should cast it to get underlying type.
+// By default, in context specific class, its try to read as tagged object, whether its explicit or implicit.
+// TODO: more robust parsing function to handle specific use cases.
+pub fn der_decode(src []u8) !Encoder {
+	tag, pos := read_tag(src, 0)!
+	length, next := decode_length(src, pos)!
+	if src.len > next + length {
+		return error('malformed bytes, contains discarded bytes')
+	}
+	// remaining is a contents
+	// contents := src[next..next + length]
+	contents := read_bytes(src, next, length)!
+	match tag.class {
+		.universal {
+			if tag.constructed {
+				return parse_compound_element(tag, contents)!
+			}
+
+			return parse_primitive_element(tag, contents)!
+		}
+		.application {
+			return new_asn_object(.application, tag.constructed, tag.number, contents)
+		}
+		.context {
+			if tag.constructed {
+				return read_explicit_context(tag, contents)!
+			}
+			return read_implicit_context(tag, contents)!
+		}
+		.private {
+			return new_asn_object(.private, tag.constructed, tag.number, contents)
+		}
+	}
+}
+
+fn parse_primitive_element(tag Tag, contents []u8) !Encoder {
+	if tag.is_constructed() {
+		return error('not primitive tag ')
+	}
+
+	match tag.number {
+		int(TagType.boolean) {
+			return read_boolean(contents)
+		}
+		int(TagType.integer) {
+			return new_integer_from_bytes(contents)
+		}
+		int(TagType.bitstring) {
+			return new_bitstring_from_bytes(contents)
+		}
+		int(TagType.octetstring) {
+			return new_octetstring(contents.bytestr())
+		}
+		int(TagType.null) {
+			return new_null()
+		}
+		int(TagType.oid) {
+			return new_oid_from_bytes(contents)!
+		}
+		int(TagType.numericstring) {
+			return new_numeric_string(contents.bytestr())
+		}
+		int(TagType.printablestring) {
+			return new_printable_string(contents.bytestr())
+		}
+		int(TagType.ia5string) {
+			return new_ia5string(contents.bytestr())
+		}
+		int(TagType.utf8string) {
+			return new_utf8string(contents.bytestr())
+		}
+		int(TagType.visiblestring) {
+			return new_visiblestring(contents.bytestr())
+		}
+		int(TagType.utctime) {
+			return new_utctime(contents.bytestr())
+		}
+		// TODO: add other type
+		else {
+			return error('unsupported tag type')
+		}
+	}
+}
+
+fn parse_compound_element(tag Tag, contents []u8) !Encoder {
+	if !tag.is_constructed() {
+		return error('not constructed tag')
+	}
+
+	match true {
+		tag.is_sequence_tag() {
+			return parse_seq(tag, contents)!
+		}
+		tag.is_set_tag() {
+			return parse_set(tag, contents)!
+		}
+		tag.is_context() {
+			return read_explicit_context(tag, contents)!
+		}
+		else {
+			return new_asn_object(tag.class, tag.constructed, tag.number, contents)
+		}
+	}
+}
+
+// Cast function.
+// Its cast encoder type to real instance type.
+
 fn (e Encoder) as_sequence() !Sequence {
 	if e is Sequence {
+		// without dereferencing, its results in error: error: fn `as_sequence` expects you to return
+		// a non reference type `!asn1.Sequence`, but you are returning `&asn1.Sequence` instead
 		return *e
 	}
-	return error('not sequence')
+	return error('not sequence type')
+}
+
+fn (e Encoder) as_set() !Set {
+	if e is Set {
+		return *e
+	}
+	return error('not set type')
 }
 
 fn (e Encoder) as_boolean() !AsnBoolean {
@@ -88,6 +207,13 @@ fn (e Encoder) as_null() !Null {
 		return *e
 	}
 	return error('not null type')
+}
+
+fn (e Encoder) as_oid() !Oid {
+	if e is Oid {
+		return *e
+	}
+	return error('not oid type')
 }
 
 fn (e Encoder) as_enumerated() !Enumerated {
@@ -144,13 +270,6 @@ fn (e Encoder) as_generalizedtime() !GeneralizedTime {
 		return *e
 	}
 	return error('not generalizedtime type')
-}
-
-fn (e Encoder) as_oid() !Oid {
-	if e is Oid {
-		return *e
-	}
-	return error('not oid type')
 }
 
 // length gets the bytes length of multi encoder.
