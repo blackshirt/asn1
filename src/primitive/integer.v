586c626814e4f0aa66c2ac057cbe9444f6aaadf8
blackshirt <blackshirt@users.noreply.github.com>
2024-03-11 08:34:15 +0700

    Update integer.v

--- src/primitive/integer.v
+++ src/primitive/integer.v
@@ -170,6 +170,7 @@ fn Integer.unpack_from_twoscomplement_bytes(b []u8) !Integer {
 	if b.len == 0 {
 		return error('Integer: null bytes')
 	}
+	// TODO: removes prepended bytes when its meet criteria
 	mut num := big.integer_from_bytes(b)
 	// negative number
 	if b.len > 0 && b[0] & 0x80 > 0 {
