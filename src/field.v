ee4898bafdae3a314ede1d19058881d2406124a7
blackshirt <blackshirt@users.noreply.github.com>
2024-09-26 08:14:25 +0000

    updates field method

--- src/field.v
+++ src/field.v
@@ -1,7 +1,52 @@
 module asn1
 
+// This file is for supporting configure through string options.
+// so, you can tag your struct field with attributes, for example @[application:10; optional; has_default]
 // Field options attributes handling
+
+// limit of string option length
 const max_string_option_length = 255
+
+@[noinit]
+struct FieldOptions {
+mut:
+	// wrapper class
+	wrapper &TagClass = unsafe { nil }
+	// set to true when should be optional element
+	optional bool
+	// set to true when optional element has default value
+	has_default bool
+	// tag number for wrapper element tagnum != nil when wrapper != nil
+	tagnum &int = unsafe { nil }
+	// default value for optional element when has_default value is true
+	default_value &Element = unsafe { nil }
+	// make sense in explicit context, when wrapper != nil and wrapper == .context_specific
+	explicit bool
+}
+
+// validate validates FieldOptions to meet criteria
+fn (fo &FieldOptions) validate() ! {
+	// if wrapper != nil, the tagnum should be provided ( != nil )
+	if fo.wrapper != unsafe { nil } && fo.tagnum == unsafe { nil } {
+		return error('non nill fo.wrapper, but fo.tagnume not specified')
+	}
+	if fo.has_default && fo.default_value == unsafe { nil } {
+		return error('fo.has_default without default_value')
+	}
+}
+
+// creates empty field options
+fn empty_field_options() &FieldOptions {
+	return &FieldOptions{}
+}
+
+// field_options_from_string parses and creates FieldOptions from string s
+fn field_options_from_string(s string) !&FieldOptions {
+	attrs := parse_string_option(s)!
+	out := parse_attrs_to_field_options(attrs)!
+	fo.validate()!
+	return fo
+}
 
 // parse_string_option parses string as an attribute of field options
 // Its allows string similar to `application:4; optional; has_default` to be treated as an field options
@@ -22,13 +67,7 @@ fn parse_string_option(s string) ![]string {
 	return out
 }
 
-fn parse_string_to_field_optipns(s string) !&FieldOptions {
-	attrs := parse_string_option(s)!
-	out := parse_attrs(attrs)!
-	return out
-}
-
-fn parse_attrs(attrs []string) !&FieldOptions {
+fn parse_attrs_to_field_options(attrs []string) !&FieldOptions {
 	validate_attrs(attrs)!
 
 	mut opts := &FieldOptions{}
@@ -46,7 +85,7 @@ fn parse_attrs(attrs []string) !&FieldOptions {
 fn validate_attrs(attrs []string) ! {
 	if attrs.len == 0 {
 		// do nothing
-		return 
+		return
 	}
 	// tagclass is present
 	tcls_present, wrapkey := attrs_has_tagclass_wrapper(attrs)
@@ -86,7 +125,8 @@ fn attrs_has_tagclass_wrapper(attrs []string) (bool, string) {
 }
 
 fn valid_tagclass_attr_name(s string) bool {
-	return s == 'application' || s == 'private' || s == 'context_specific' || s == 'universal'
+	return src == 'application' || src == 'private' || src == 'context_specific'
+		|| src == 'universal'
 }
 
 fn valid_tagclass_attr_number(s string) bool {
@@ -96,7 +136,7 @@ fn valid_tagclass_attr_number(s string) bool {
 // valid tag class 'application: 5' format
 fn valid_tagclass_format(attr string) bool {
 	if attr.starts_with('application') || attr.starts_with('context_specific')
-		|| attr.starts_with('private') {
+		|| attr.starts_with('private') || attr.starts_with('universal') {
 		res := attr.split(';')
 		// should be in 'application:number' format
 		if res.len != 2 {
@@ -139,16 +179,12 @@ fn tag_class_and_number(s string) !(TagClass, u32) {
 	if !valid_tagclass_attr_name(tc) {
 		return error('Not valid class name')
 	}
-	tclass := tag_class_from_string(tc)!
 	if !valid_tagclass_attr_number(tn) {
 		return error('not valid tag num format')
 	}
-	if tn.is_int() {
-		tnum := tn.u32()
-		return tclass, tnum
-	}
+	tcls := tag_class_from_string(tc)!
 	tnum := tagnum_from_int(tn.int())!
-	return tclass, tnum
+	return tcls, tnum
 }
 
 // is_element check whethers T is fullfills Element
@@ -156,3 +192,59 @@ fn is_element[T]() bool {
 	s := $if T is Element { true } $else { false }
 	return s
 }
+
+fn has_tag_method[T]() bool {
+	$for method in T.methods {
+		$if method.name == 'tag' {
+			$if method.return_type is Tag {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+fn make_payload[T]() ![]u8 {
+}
+
+fn encode_element(el Element) ![]u8 {
+	return encode_element_with_options(el, '')!
+}
+
+fn encode_element_with_options(el Element, opts string) ![]u8 {
+	opt := field_options_from_string(opts)!
+	mut out := []u8{}
+	el.encode_with_options(mut out, opt)!
+	return out
+}
+
+fn (el Element) encode_with_options(opt &FieldOptions) ![]u8 {
+	opt.validate()!
+	out := []u8{}
+	if opt.optional {
+		if opt.present {
+			// make optional object from element
+			obj := make_optional_from_element(el)!
+			// is this need wrapped ?
+			if opt.tagclass != unsafe { nil } {
+				if el.tag().tag_class() == opt.tagclass {
+					// no need to wrap
+					return
+				}
+				// different tag class..wraps it
+				wrapped_obj := wrap_element(obj, opt.tagclass, opt.tagnum, true)!
+				wrapped_obj.encode(mut out)!
+				return out
+			}
+			//
+			obj.encode(mut out)!
+			return out
+		}
+	}
+	// not an optional element
+	el.encode(mut out)!
+
+	return out
+}
+
+fn make_optional_from_element(el Element) ()
--- src/optional.v
+++ src/optional.v
@@ -2,6 +2,12 @@ module asn1
 
 // OPTIONAL
 //
+// note: At the abstract ASN.1 level the absence of a DEFAULT value in an encoding is the same as its being present.
+// Contrast this with OPTIONAL, where a value being present in the encoding is semantically distinct from its being absent.
+// In some encoding rules (like BER/PER) it is at the whim of the sender whether a DEFAULT value is encoded or not
+// (except for primitive type values in PER which are required by the PER standard to be absent in the encoding),
+// while with others (like DER) the DEFAULT value is NEVER encoded. For all encoding rules,
+// if the component that has a DEFAULT value is not encoded the receiving application must behave as though the DEFAULT value had been encoded.
 struct Optional[T] {
 	// set to true when its should present
 	present bool
@@ -11,19 +17,37 @@ struct Optional[T] {
 	default_value &T = unsafe { nil }
 }
 
+fn validate_optional(opt Optional[T]) ! {
+	if opt.has_default {
+		if opt.default_value == unsafe { nil } {
+			return error('Optional has default_value, but default_value is nil')
+		}
+	}
+}
+
 fn Optional.from_element(el Element) Optional {
 	return Optional{
 		el: el
 	}
 }
 
-fn (o Optional) tag() ?Tag {
-	t := o.el or { return none }
-	return t.tag()
+fn (opt Optional) tag() Tag {
+	return opt.el.tag()
 }
 
-fn (o Optional) payload() ![]u8 {
-	el := o.el or { return []u8{cap: 0} }
+fn (opt Optional) payload() ![]u8 {
+	return opt.el.payload()!
+}
 
-	return el.payload()!
+fn (opt Optional) encode() ![]u8 {
+	mut out := []u8{}
+	// when its not present, its should be encoded into empty bytes
+	if !opt.present {
+		return out
+	}
+	// when present, its should be serializable
+	opt.tag().encode(mut out)!
+	out << opt.payload()!
+
+	return out
 }
