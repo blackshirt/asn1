de693ab198d3a042c434642b252bd04d5e266b20
blackshirt <blackshirt@users.noreply.github.com>
2024-10-15 09:51:40 +0700

    updates optional

--- src/optional.v
+++ src/optional.v
@@ -8,18 +8,22 @@ module asn1
 // (except for primitive type values in PER which are required by the PER standard to be absent in the encoding),
 // while with others (like DER) the DEFAULT value is NEVER encoded. For all encoding rules,
 // if the component that has a DEFAULT value is not encoded the receiving application must behave as though the DEFAULT value had been encoded.
-@[heap]
-struct Optional {
+@[noinit; heap]
+pub struct Optional {
 	// underlying element marked as an optional
-	elem Element
+	tag     Tag
+	content []u8
 mut:
 	// presence of this flag negates optionality of this elemeent.
 	// set to true when its should present, if notu sure, just set to to false
 	present bool
+	// set to none when have no default
+	default_value ?Element
 }
 
 fn (opt Optional) validate() ! {
-	/* if opt.has_default && opt.default_value == none {
+	/* 
+	if opt.has_default && opt.default_value == none {
 		return error('Optional with has_default but default_value is none')
 	}
 	if opt.has_default && opt.default_value != none {
@@ -27,67 +31,83 @@ fn (opt Optional) validate() ! {
 		if opt.elem.tag() != val.tag() {
 			return error('default value with different tag is not allowed')
 		}
-	} */
+	} 
+	*/
 }
 
-fn new_optional(el Element) Optional {
+pub fn new_optional(el Element) !Optional {
 	return Optional{
-		elem: el
+		tag: el.tag()
+		content: el.payload()!
 	}
 }
 
-/* fn (mut opt Optional) set_default(el Element) ! {
-	if !opt.has_default {
-		return
-	}
-	if opt.elem.tag() != el.tag() {
+pub fn (mut opt Optional) set_default(el Element) ! {
+	if !opt.tag.equal(el.tag()) {
 		return error('default value with different tag is not allowed')
 	}
 	opt.default_value = el
 }
 
-fn (mut opt Optional) with_default(el Element) !&Optional {
+pub fn (mut opt Optional) with_default(el Element) !&Optional {
 	opt.set_default(el)!
 	return opt
 }
 
-fn (mut opt Optional) with_has_default(flag bool) &Optional {
-	opt.has_default = flag
-	return opt
-} 
-*/
-
-fn (mut opt Optional) with_present(present bool) Optional {
+pub fn (mut opt Optional) with_present(present bool) Optional {
 	opt.present = present
 	return opt
 }
 
-fn (opt Optional) tag() Tag {
-	return opt.elem.tag()
+pub fn (opt Optional) tag() Tag {
+	return opt.tag()
 }
 
-fn (opt Optional) payload() ![]u8 {
+pub fn (opt Optional) payload() ![]u8 {
 	// opt.validate()!
-	return opt.elem.payload()!
+	return opt.content
+}
+
+pub fn (opt Optional) encode() ![]u8 {
+	return opt.encode_with_rule(.der)!
 }
 
 fn (opt Optional) encode_with_rule(rule EncodingRule) ![]u8 {
 	if opt.present {
-		return encode_with_rule(opt.elem, .der)!
+		elem := Asn1Element{tag: opt.tag, content: opt.content}
+		return encode_with_rule(elem, .der)!
 	}
 	// not present
 	return []u8{}
 }
 
-fn (opt Optional) into_t[T]() !T {
+fn (opt Optional) into_element() !Element {
+    match tag.class {
+		.universal {
+			return parse_universal(opt.tag, opt.content)!
+		}
+		.application {
+			return parse_application(opt.tag, opt.content)!
+		}
+		.context_specific {
+			return parse_context_specific(opt.tag, opt.content)!
+		}
+		.private {
+			return parse_private(opt.tag, opt.content)!
+		}
+	}
+}
+
+pub fn (opt Optional) into_t[T]() !T {
 	$if T !is Element {
 		return error('T is not element')
 	}
 	$if T is Optional {
 		return error('T is optional')
 	}
-	if opt.elem is Optional {
+	elem := opt.into_element()!
+	if elem is Optional {
 		return error('Optional.elem is also optional')
 	}
-	return opt.elem.into_object[T]()!
+	return elem.into_object[T]()!
 }
