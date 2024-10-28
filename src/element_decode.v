f9a125029eb6a61774b01a7c564567d77885926c
blackshirt <blackshirt@users.noreply.github.com>
2024-10-28 03:57:16 +0000

    updates element_decode

--- src/element_decode.v
+++ src/element_decode.v
@@ -80,38 +80,84 @@ fn decode_optional(bytes []u8, expected_tag Tag) !Element {
 	return opt
 }
 
-fn (el Element) unwrap_with_options(fo FieldOptions) !Element {
-	el.validate_options(fo)!
 
-	// if unwrapping, el.tag() should == fo.inner produced by wrap operation
-	return error('Not implemented')
-}
-
-// unwrap performs unwrapping operations to the element with options provided.
+// unwrap_with_options performs unwrapping operations to the element with options provided.
 // Its technically reverse operation of the `.wrap()` applied to the element
 // with the same options. If you provide with diferent options,
-// the result is in undesired behaviour, even its success 
-fn unwrap(el Element, fo FieldOptions) !Element {
+// the result is in undesired behaviour, even its success
+fn (el Element) unwrap_with_options(fo FieldOptions) !Element {
+	if fo.cls == '' {
+		// no unwrap 
+		return el 
+	}
 	fo.validate_options()!
-	// first, checks class of the element being to unwrap
+	// first, checks class of the element being to unwrap, should not universal class.
 	if el.tag().class == .universal {
 		return error('you cant unwrap universal element')
+	}
+	// its also happens to fo.cls, should not universal class 
+	if fo.cls 
+	// if optional 
+	if fo.optional {
+		opt := decode_optional(el.payload()!, el.tag())!
+		return opt 
 	}
 	// element being unwrap should have matching with tag within options.
 	mode := TaggedMode.from_string(fo.mode)!
 	inner_form := false
-	constructed := if mode == .explicit {true} else {inner_form}
+	constructed := if mode == .explicit { true } else { inner_form }
 	tg := Tag.new(fo.cls, constructed, fo.tagnum)!
 
-	//check for class
-	cls := TagClass.from_string(go.cls)!
+	// check for class
+	cls := TagClass.from_string(fo.cls)!
 	if el.tag().class != cls {
-		return error('unmatching tag class'}
+		return error('unmatching tag class')
 	}
-		
-	// check tag equality 
+
+	// check tag equality
 	if !el.tag().equal(tg) {
 		return error('Element tag unequal with tag from options')
 	}
+}
+
+// unwrap the provided element.
+fn unwrap(el Element, cls TagClass, number int, mode TaggedMode, inner_tag Tag) !Element {
+	if el.tag().class != cls {
+		return error('unwrap: unmatching tag class')
+	}
+	if cls == .universal || el.tag().class == .universal {
+		return error('unwrap: unalllowed universal class')
+	}
+	if mode == .explicit {
+		if !el.tag().constructed {
+			return error('explicit mode should have constructed tag')
+		}
+			// checks inner tag from payload 
+		tag, _ := Tag.decode_with_rule(el.payload()!, 0, .der)!
+		if !tag.equal(inner_tag) {
+			return asn1_error(.unexpected_tag_value, 'Get unexpected inner tag from payload')!
+		}
+	}
+	// the form taken from current element being wrapped 
+	form := el.tag().constructed 	
+	constructed := if mode == .explicit { true } else { form }
+	outer_tag := Tag.new(cls, constructed, number)!
+	
+	// This outer_tag should equal with current elements tag.
+	if !outer_tag.equal(el.tag()) {
+		return error('unwrap: unmatching tag found')
+	}
+
+	match cls {
+		.context_specific {
+
+		}
+		.application {}
+		.private {}
+		else {
+			return error('unwrap: Wrong class')
+		}
+	}
+
 	
 }
